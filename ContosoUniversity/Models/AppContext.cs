53f7e5215374e1127cd7a00a0422fba5cc00d72a
acambas <aleksandarcambas@gmail.com>
2013-11-06 15:57:36 +0100

    first model

---
+++ ContosoUniversity/Models/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class AppContext:DbContext
+    {
+        public AppContext()
+            :base("DefaultConnection")
+        {}
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
